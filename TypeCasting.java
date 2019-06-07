387fe1d86e63a250e94f96af21cdc604e63bd9bc
Fayaz Mohammad <fayazfz07@gmail.com>
2019-06-07 09:19:42 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting
+{
+	public static void main(String[] args) {
+        int i = 1024;
+        float f = (float)i;
+        System.out.println("Type casting " + i + "\nInt as Float: " + (float)i + "\nInt as Double: "+(double)i+"\nInt as char: " +(char)i);
+	}
+}
