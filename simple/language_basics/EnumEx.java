6b9eddc5278f6d3766af1dd9a4dbd74757e2de72
Fayaz Mohammad <fayazfz07@gmail.com>
2019-06-09 23:07:51 +0530

    Create EnumEx.java

---
+++ simple/language_basics/EnumEx.java
@@ -0,0 +1,14 @@
+enum Color 
+{ 
+    RED, GREEN, BLUE; 
+} 
+  
+public class EnumEx 
+{ 
+    // Driver method 
+    public static void main(String[] args) 
+    { 
+        Color c1 = Color.RED; 
+        System.out.println(c1); 
+    } 
+}
